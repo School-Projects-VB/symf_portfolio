870560d720fd499fb10518bcea92c3cd45038a62
ReyFey <vic.betsch@gmail.com>
2021-09-16 14:01:37 +0200

    ✨ create pages

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,10 +10,33 @@ class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="index")
-     * @throws Exception
      */
     public function index(): Response
     {
         return $this->render('index.html.twig');
+    }
+
+    /**
+     * @Route("/cv", name="cv")
+     */
+    public function cv(): Response
+    {
+        return $this->render('cv.html.twig');
+    }
+
+    /**
+     * @Route("/projects", name="projects")
+     */
+    public function projects(): Response
+    {
+        return $this->render('projects.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig');
     }
 }
